3c8249bf4f98d447894e18c2a4406994a287d3af
BuildTools <olmorris@ucalgary.ca>
2021-11-16 20:59:11 -0700

    Initial commit

---
+++ Assets/GameMaster.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameMaster : MonoBehaviour {
+
+    public const string allChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private Random rand;
+
+    // Start is called before the first frame update
+    void Start() {
+        Random rand = new Random();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    public void Step() {
+
+    }
+
+    public char RandomLetter() {
+        
+
+        return 
+
+    }
+}
